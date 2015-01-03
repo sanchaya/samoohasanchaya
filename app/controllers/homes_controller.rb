159ea6b4a8d703c67bb6762186f787d53f89e7f7
devudilip <devu.dilip@gmail.com>
2015-01-04 04:40:09 +0530

    count xirxle box implemented

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
   def index
+    @translated_authors_count = AuthorTranslation.count
+    @translated_publishers_count = PublisherTranslation.count
+    @translated_books_count = BookTranslation.count
   end
 end
