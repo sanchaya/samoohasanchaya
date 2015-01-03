400eb9a7387026d99957090dc1a681cfe9c1c89e
devudilip <devu.dilip@gmail.com>
2015-01-04 03:31:44 +0530

    publishers index updated as same as book

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,4 +1,7 @@
 class PublishersController < ApplicationController
   def index
+    translated = PublisherTranslation.pluck('publisher_id')
+    untranslated = Publisher.where("id not in (?)", translated.blank? ? [0] : translated )
+    @publishers = untranslated.paginate(:page => params[:page])
   end
 end
