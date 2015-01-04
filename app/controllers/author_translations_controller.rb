4e8d5b863b1e4e96e363f4751478b16dfc466d0a
devudilip <devu.dilip@gmail.com>
2015-01-05 00:44:03 +0530

    yaml files added for translations

--- app/controllers/author_translations_controller.rb
+++ app/controllers/author_translations_controller.rb
@@ -1,4 +1,5 @@
 class AuthorTranslationsController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
 
--- app/controllers/publisher_translations_controller.rb
+++ app/controllers/publisher_translations_controller.rb
@@ -1,4 +1,5 @@
 class PublisherTranslationsController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
 
