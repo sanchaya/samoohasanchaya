fb2ae302578a89c99b98eeb26274085d1980c9fd
devudilip <devu.dilip@gmail.com>
2015-01-12 00:33:53 +0530

    Jquery validation, ime for kannada yaml files updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,67 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require jquery.validate
+//= require jquery.ime
+//= require jquery.ime.selector
+//= require jquery.ime.preferences
+//= require jquery.ime.inputmethods
 //= require_tree .
+
+
+$( document ).ready( function () {
+	// Kannada ime enabled for form 
+	$( '.kan-ime' ).ime();
+
+// validatin added for book translation form
+$("#new_book_translation").validate({
+	rules:{
+
+		"book_translation[book_title]":
+		{
+			required: true
+		}
+	},
+	messages:{
+		"book_translation[book_title]":
+		{
+			required: "ದಯವಿಟ್ಟು ಪದವನ್ನು ಬೆರಳಚ್ಚು ಮಾಡಿ"
+		}
+	}
+});
+
+// validatin added for publisher translation form
+$("#new_publisher_translation").validate({
+	rules:{
+
+		"publisher_translation[name]":
+		{
+			required: true
+		}
+	},
+	messages:{
+		"publisher_translation[name]":
+		{
+			required: "ದಯವಿಟ್ಟು ಪದವನ್ನು ಬೆರಳಚ್ಚು ಮಾಡಿ"
+		}
+	}
+});
+
+// validatin added for author translation form
+$("#new_author_translation").validate({
+	rules:{
+
+		"author_translation[name]":
+		{
+			required: true
+		}
+	},
+	messages:{
+		"author_translation[name]":
+		{
+			required: "ದಯವಿಟ್ಟು ಪದವನ್ನು ಬೆರಳಚ್ಚು ಮಾಡಿ"
+		}
+	}
+});
+
+});
