c2ce09a30ffee6686ef21e6c83f189e63f88a4ce
devudilip <devu.dilip@gmail.com>
2015-01-14 02:04:00 +0530

    devise session store fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_samooha_session'
-config.secret_key = '5b1bf43a4ebff7f03e584821c64f01a59d413bccf4b1a09bf606bd52db9596d59b48bd7a22fdbc10ff2efaaabc03ca4012336483a554afbd73ca14cd9eb28595'
+Samooha::Application.config.secret_key = '5b1bf43a4ebff7f03e584821c64f01a59d413bccf4b1a09bf606bd52db9596d59b48bd7a22fdbc10ff2efaaabc03ca4012336483a554afbd73ca14cd9eb28595'
