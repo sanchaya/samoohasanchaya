19443afe890c621ab15b806a52ace37f9460a240
devudilip <devu.dilip@gmail.com>
2015-01-17 02:45:37 +0530

    highcharts dnut added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require jquery.validate
+//= require highcharts
 //= require jquery.ime
 //= require jquery.ime.selector
 //= require jquery.ime.preferences
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,15 +8,18 @@ class HomesController < ApplicationController
     # Move logics to Model
     # Use single query to fetch untranslated
     translated_books = BookTranslation.pluck('book_id')
-    @book = Book.where("id not in (?)", translated_books.blank? ? [0] : translated_books ).first
+    @books = Book.where("id not in (?)", translated_books.blank? ? [0] : translated_books )
+    @book = @books.first
     @translate_book = @book.book_translations.new
 
     translated_publishers = PublisherTranslation.pluck('publisher_id')
-    @publisher = Publisher.where("id not in (?)", translated_publishers.blank? ? [0] : translated_publishers ).first
+    @publishers = Publisher.where("id not in (?)", translated_publishers.blank? ? [0] : translated_publishers )
+    @publisher = @publishers.first
     @translate_publisher = @publisher.publisher_translations.new
 
     translated_authors = AuthorTranslation.pluck('author_id')
-    @author = Author.where("id not in (?)", translated_authors.blank? ? [0] : translated_authors ).first
+    @authors = Author.where("id not in (?)", translated_authors.blank? ? [0] : translated_authors )
+    @author = @authors.first
     @translate_author = @author.author_translations.new
   end
 end
