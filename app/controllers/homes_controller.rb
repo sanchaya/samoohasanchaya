5dddd9a0bdac905301e8aa55e3b290b0083db674
devudilip <devu.dilip@gmail.com>
2015-01-23 02:49:42 +0530

    rand of records to edit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,17 +8,17 @@ class HomesController < ApplicationController
     # Move logics to Model
     # Use single query to fetch untranslated
     translated_books = BookTranslation.pluck('book_id')
-    @books = Book.where("id not in (?)", translated_books.blank? ? [0] : translated_books )
+    @books = Book.where("id not in (?)", translated_books.blank? ? [0] : translated_books ).order('RAND()')
     @book = @books.first
     @translate_book = @book.book_translations.new
 
     translated_publishers = PublisherTranslation.pluck('publisher_id')
-    @publishers = Publisher.where("id not in (?)", translated_publishers.blank? ? [0] : translated_publishers )
+    @publishers = Publisher.where("id not in (?)", translated_publishers.blank? ? [0] : translated_publishers ).order('RAND()')
     @publisher = @publishers.first
     @translate_publisher = @publisher.publisher_translations.new
 
     translated_authors = AuthorTranslation.pluck('author_id')
-    @authors = Author.where("id not in (?)", translated_authors.blank? ? [0] : translated_authors )
+    @authors = Author.where("id not in (?)", translated_authors.blank? ? [0] : translated_authors ).order('RAND()')
     @author = @authors.first
     @translate_author = @author.author_translations.new
 end
