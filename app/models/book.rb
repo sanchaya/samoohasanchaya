7abc31ae688ae64a1143f612d82618b2e67c1326
devudilip <devu.dilip@gmail.com>
2015-08-16 00:22:29 +0530

    master view created for books search

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,8 +6,17 @@ class Book < ActiveRecord::Base
   belongs_to :publisher
 
   def self.search_books keyword
+    ActiveRecord::Base.connection.execute("
+        select * from master_books
+        where name like '%#{keyword}%'
+        or author like '%#{keyword}%'
+        ")
+end
+
+def self.create_master 
    # BookTranslation.joins(:book, :book_description).select("book_translations.book_title AS book_title, books.id AS book_id, book_descriptions.link AS link").where("book_translations.book_title like ?", "%#{keyword}%")
    ActiveRecord::Base.connection.execute("
+    CREATE VIEW master_books AS
     select bt.book_title as name,at.name as author,pt.name as publisher, 'osmania' as library,  bd.link from book_translations bt
     inner join books b
     on bt.book_id = b.id
@@ -21,8 +30,6 @@ class Book < ActiveRecord::Base
     on p.id = b.publisher_id
     left join publisher_translations pt
     on pt.publisher_id = p.id
-    where bt.book_title like '%#{keyword}%'
-    or at.name like '%#{keyword}%'
 
     UNION ALL
     select dbt.book_title as name,dat.name as author,dpt.name as publisher,'dli' as library, dbd.link from dli_book_translations dbt
@@ -38,9 +45,6 @@ class Book < ActiveRecord::Base
     on dp.id = db.publisher_id
     left join dli_publisher_translations dpt
     on dpt.publisher_id = dp.id
-    where dbt.book_title like '%#{keyword}%'
-
-    order by name
     ")
 
 end
