fc424fead185b56b08ab2a1f3657f5374f4275b8
devudilip <devu.dilip@gmail.com>
2015-11-16 01:01:41 +0530

    user authentication added

--- app/controllers/fuel_translations_controller.rb
+++ app/controllers/fuel_translations_controller.rb
@@ -1,10 +1,10 @@
 class FuelTranslationsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   def create
     @word = FuelWord.find(params[:fuel_word_id])
     @word_translate = @word.fuel_translations.new(fuel_translation_params)
     @word_translate.language_id = Language.kannada.id
-    # @word_translate.user_id = current_user.id
+    @word_translate.user_id = current_user.id
     if @word_translate.save
      redirect_to root_path, notice: 'ಪುಸ್ತಕದ ಹೆಸರನ್ನು ಯಶಸ್ವಿಯಾಗಿ ಕನ್ನಡೀಕರಿಸಲಾಯ್ತು.'
    else
